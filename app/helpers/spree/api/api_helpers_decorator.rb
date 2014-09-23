593fa6b6ea536f14ba6a1c458eb874788425ab45
Kyle Van Wagenen <kvanwagenen@gmail.com>
2014-09-23 15:58:45 -0600

    Fix issue with class variables

--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -2,9 +2,9 @@ Spree::Api::ApiHelpers.class_eval do
 
   alias_method :orig_order_attributes, :order_attributes
 
-  @@order_attributes = []
+  @order_attributes = []
   def self.extended_order_attributes
-    @@order_attributes
+    @order_attributes
   end
   def order_attributes
     orig_order_attributes + Spree::Api::ApiHelpers.extended_order_attributes
