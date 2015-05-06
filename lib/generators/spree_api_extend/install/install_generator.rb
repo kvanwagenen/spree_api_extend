1df99ac0f4abc0c9aa9f7d2ad671d0d8a822787c
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-05-06 16:39:35 -0600

    Remove unnecessary assets

--- lib/generators/spree_api_extend/install/install_generator.rb
+++ lib/generators/spree_api_extend/install/install_generator.rb
@@ -4,16 +4,6 @@ module SpreeApiExtend
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_api_extend\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_api_extend\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_api_extend\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_api_extend\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_api_extend'
       end
